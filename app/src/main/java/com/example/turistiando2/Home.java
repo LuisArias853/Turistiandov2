b9e4cc52f35bcc58e043f729c83fe89775ba043c
207 <207@academicos.cesde.edu.co>
2023-07-04 20:38:59 -0500

    Se crea menu de opciones

--- app/src/main/java/com/example/turistiando2/Home.java
+++ app/src/main/java/com/example/turistiando2/Home.java
@@ -3,6 +3,8 @@ package com.example.turistiando2;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
@@ -19,15 +21,50 @@ public class Home extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
 
-        botonHoteles=findViewById(R.id.buttonhoteles);
-        botonRestaurantes=findViewById(R.id.buttonrestaurantes);
-        botonLugares=findViewById(R.id.buttonlugares);
-    //DETECTANDO EVENTOS
-        botonHoteles.setOnContextClickListener(new View.OnContextClickListener() {
+        botonHoteles = findViewById(R.id.buttonhoteles);
+        botonRestaurantes = findViewById(R.id.buttonrestaurantes);
+        botonLugares = findViewById(R.id.buttonlugares);
+        //DETECTANDO EVENTOS
+        botonHoteles.setOnClickListener(new View.OnClickListener() {
             @Override
-            public boolean onContextClick(View v) {
-                Toast.makeText(Home.this)
+            public void onClick(View v) {
+                Toast.makeText(Home.this, "Hice click 1", Toast.LENGTH_SHORT).show();
             }
         });
+
+        botonRestaurantes.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(Home.this, "Hice click 2", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        botonLugares.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(Home.this, "Hice click 3", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+
+    }
+    public boolean onCreateOptionsMenu(Menu menu){
+        getMenuInflater().inflate(R.menu.menu,menu);
+        return true;
+    }
+
+    public boolean onOptionItemSelected(MenuItem item){
+        int itemSeleccionado=item.getItemId();
+        switch (itemSeleccionado){
+            case(R.id.opcion1):
+                break;
+            case(R.id.opcion2):
+                break;
+            case(R.id.opcion3):
+                break;
+            case(R.id.opcion4):
+                break;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
