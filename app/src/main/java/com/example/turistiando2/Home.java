f1a8841076e059ba6ec8151af650359df61066dc
Elizabeth <lenguaje.pecz@gmail.com>
2023-07-19 20:50:10 -0500

    Penultimaclase

--- app/src/main/java/com/example/turistiando2/Home.java
+++ app/src/main/java/com/example/turistiando2/Home.java
@@ -3,6 +3,7 @@ package com.example.turistiando2;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.content.res.Configuration;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -10,6 +11,8 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import java.util.Locale;
 
 public class Home extends AppCompatActivity {
     //ZONA DE LOS ATRIBUTOS
@@ -62,32 +65,54 @@ public class Home extends AppCompatActivity {
 
 
     }
-    public boolean onCreateOptionsMenu(Menu menu){
-        getMenuInflater().inflate(R.menu.menu,menu);
+
+    public void cambiarIdioma(String idioma){
+        //configurar el lenguaje del telefono
+        Locale lenguaje=new Locale(idioma);
+        Locale.setDefault(lenguaje);
+
+        //Configuracion globalmente el telefono
+        Configuration configuracionTelefono=getResources().getConfiguration();
+        configuracionTelefono.locale=lenguaje;
+        // Ejecutamos la configuracion
+        getBaseContext().getResources().updateConfiguration(configuracionTelefono,getBaseContext().getResources().getDisplayMetrics());
+    }
+
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu, menu);
         return true;
     }
 
-    public boolean onOptionItemSelected(MenuItem item){
+    public boolean onOptionsItemSelected(MenuItem item){
         int itemSeleccionado=item.getItemId();
         switch (itemSeleccionado){
+
             case(R.id.opcion1):
-                Toast.makeText(this, "quiero que este en español", Toast.LENGTH_SHORT).show();
+                this.cambiarIdioma("en");
+                Intent intentIngles=new Intent(Home.this,Home.class);
+                startActivity(intentIngles);
                 break;
             case(R.id.opcion2):
-                Toast.makeText(this, "quiero que este ingles", Toast.LENGTH_SHORT).show();
+                this.cambiarIdioma("es");
+                Intent intentEspañol=new Intent(Home.this,Home.class);
+                startActivity(intentEspañol);
                 break;
             case(R.id.opcion3):
-                Toast.makeText(this, "quiero que este en italiano", Toast.LENGTH_SHORT).show();
+                this.cambiarIdioma("it");
+                Intent intentItaliano=new Intent(Home.this,Home.class);
+                startActivity(intentItaliano);
                 break;
             case(R.id.opcion4):
-                Toast.makeText(this, "quiero que este en acerca de", Toast.LENGTH_SHORT).show();
+                Intent intentAcercade=new Intent(Home.this,acercade.class);
+                startActivity(intentAcercade);
                 break;
+
         }
+
         return super.onOptionsItemSelected(item);
     }
 
-    public void cambiarIdioma(String idioma){
 
-    }
+
 
 }
