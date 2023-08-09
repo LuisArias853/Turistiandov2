bccdf521435b8b6c5f1a7ba6315e1fa0c7eafe72
smarinv16 <kevindaza13@gmail.com>
2023-08-08 21:01:27 -0500

    Se crea scroll y moldes

--- app/src/main/java/com/example/turistiando2/moldes/MoldeRestaurante.java
+++ app/src/main/java/com/example/turistiando2/moldes/MoldeRestaurante.java
@@ -1,2 +1,60 @@
-package com.example.turistiando2.moldes;public class MoldeRestaurante {
+package com.example.turistiando2.moldes;
+
+public class MoldeRestaurante {
+    private String nombre;
+    private String precio;
+    private String telefono;
+    private String plato;
+    private Integer foto;
+
+    public MoldeRestaurante() {
+    }
+
+    public MoldeRestaurante(String nombre, String precio, String telefono, String plato, Integer foto) {
+        this.nombre = nombre;
+        this.precio = precio;
+        this.telefono = telefono;
+        this.plato = plato;
+        this.foto = foto;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(String precio) {
+        this.precio = precio;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+    public String getPlato() {
+        return plato;
+    }
+
+    public void setPlato(String plato) {
+        this.plato = plato;
+    }
+
+    public Integer getFoto() {
+        return foto;
+    }
+
+    public void setFoto(Integer foto) {
+        this.foto = foto;
+    }
 }
