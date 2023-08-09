b32bdb78d9fe72c4ee2cd55f6cca876e90e54a2e
smarinv16 <kevindaza13@gmail.com>
2023-08-08 20:57:08 -0500

    Se crean los moldes y el scroll

---
+++ app/src/main/java/com/example/turistiando2/listaRestaurantes.java
@@ -0,0 +1,14 @@
+package com.example.turistiando2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class listaRestaurantes extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_lista_restaurantes);
+    }
+}
---
+++ app/src/main/java/com/example/turistiando2/moldes/MoldeTurismo.java
@@ -0,0 +1,2 @@
+package com.example.turistiando2.moldes;public class MoldeTurismo {
+}
