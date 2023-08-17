2ed8f4182822c8a144ad789cd988bc69c426ca86
smarinv16 <kevindaza13@gmail.com>
2023-08-16 20:02:47 -0500

    Se crea adaptador sitios turisticos

---
+++ app/src/main/java/com/example/turistiando2/adaptadores/AdaptatorSitiosTuristicos.java
@@ -0,0 +1,73 @@
+package com.example.turistiando2.adaptadores;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.turistiando2.R;
+import com.example.turistiando2.moldes.MoldeTurismo;
+import com.example.turistiando2.moldes.Moldehotel;
+
+import java.util.ArrayList;
+
+public class AdaptatorSitiosTuristicos  extends RecyclerView.Adapter<AdaptatorSitiosTuristicos.viewHolder>{
+
+    //Todos adaptador tine una lista de elementos
+    public ArrayList<MoldeTurismo> listaSitiosturisticos;
+
+    //creamo un constructor lleno vacio
+    public AdaptatorSitiosTuristicos() {
+    }
+
+
+    //creamos un constructor lleno
+    public AdaptatorSitiosTuristicos(ArrayList<MoldeTurismo> listaSitiosTuristicos) {
+        this.listaSitiosturisticos = listaSitiosTuristicos;
+    }
+
+    @NonNull
+    @Override
+    public AdaptatorSitiosTuristicos.viewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+
+        //Esta porcion de codigo permite crear n copias del molde grafico
+        View vista = LayoutInflater.from(parent.getContext()).inflate(R.layout.moldeturismo,null,false);
+        return new viewHolder(vista);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull AdaptatorSitiosTuristicos.viewHolder holder, int position) {
+        holder.actualizarDatos(listaSitiosturisticos.get(position));
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return listaSitiosturisticos.size();
+    }
+
+    public class viewHolder extends RecyclerView.ViewHolder {
+        ImageView fotoTurismo;
+        TextView nombreTurismo;
+        TextView precioTurismo;
+        TextView contactoTurismo;
+        public viewHolder(@NonNull View itemView) {
+            super(itemView);
+            fotoTurismo=itemView.findViewById(R.id.imgsitios);
+            nombreTurismo =itemView.findViewById(R.id.nombresitios);
+            precioTurismo =itemView.findViewById(R.id.preciositios);
+            contactoTurismo =itemView.findViewById(R.id.contactositios);
+        }
+
+        public  void actualizarDatos(MoldeTurismo moldeTurismo) {
+            fotoTurismo.setImageResource(moldeTurismo.getFoto());
+            nombreTurismo.setText(moldeTurismo.getNombre());
+            precioTurismo.setText(moldeTurismo.getPrecio());
+            contactoTurismo.setText(moldeTurismo.getTelefono());
+        }
+    }
+}
